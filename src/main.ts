dbf27fa44f9257338766890cb431e2c74f1019af
Manuel Cañete <manuel.canete@docplanner.com>
2023-03-23 19:44:49 +0100

    feat: parse yaml to outputs

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as yaml from 'js-yaml';
+import * as fs from 'fs';
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const filePath: string = core.getInput('filePath');
+    const fileContents = fs.readFileSync(filePath, 'utf8');
+    const data : any = yaml.load(fileContents);
+    for (const property in data) {
+      core.setOutput(property, data[property]);
+    }
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error)
+      core.setFailed(error.message);
   }
 }
 
-run()
+run();
