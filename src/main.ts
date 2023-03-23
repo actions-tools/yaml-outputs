d3f72738635d43b754a860fd10029f33a63e1c01
Manuel Cañete <manuel.canete@docplanner.com>
2023-03-23 19:44:49 +0100

    feat: parse yaml to outputs

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,50 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as fs from 'fs'
+import * as yaml from 'js-yaml'
 
-async function run(): Promise<void> {
+function run(): void {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    // Get input parameters
+    const filePath = core.getInput('filePath')
+    const separator = core.getInput('separator')
+    const exportEnvVariables = core.getBooleanInput('exportEnvVariables')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // Read file content and parse it as YAML
+    const fileContent = fs.readFileSync(filePath, 'utf8')
+    const yamlData: any = yaml.load(fileContent)
 
-    core.setOutput('time', new Date().toTimeString())
+    // Flatten the object recursively
+    const result = flattenObject(yamlData, {}, '', separator)
+
+    // Set the output parameters
+    for (const key of Object.keys(result)) {
+      core.setOutput(key, result[key])
+
+      if (exportEnvVariables) {
+        core.exportVariable(key, result[key].toString())
+      }
+    }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
+}
+
+export function flattenObject(
+  obj: any,
+  result: any,
+  prefix = '',
+  separator = '__'
+): any {
+  for (const key of Object.keys(obj)) {
+    if (typeof obj[key] === 'object') {
+      flattenObject(obj[key], result, `${prefix}${key}${separator}`, separator)
+      continue
+    }
+
+    result[prefix + key] = obj[key]
+  }
+
+  return result
 }
 
 run()
